8d487e828c023c1364b83c93b1b19633adc7c455
Makkuhuqueriza <mhpena@up.edu.ph>
2024-05-12 00:09:40 +0800

    Added UML Lab

--- UML Diagram of Class/Book.h
+++ UML Diagram of Class/Book.h
@@ -1,26 +1,27 @@
-#include <iostream>
+#include <iostream> // Include necessary header for input and output operations
 
-class Book {
+class Book { // Define a class named Book
 
-    private: 
-        //Member variables
+    private: // Define private access specifier for member variables
+        // Member variables to store book details
         std::string Title;
         std::string Author;
         std::string ISBN;
         std::string Publisher;
 
-    public: 
-        //Constructor
+    public: // Define public access specifier for member functions
+    
+        // Constructor to initialize member variables
         Book(std::string title, std::string author, std::string isbn, std::string publisher)
         : Title(title), Author(author), ISBN(isbn), Publisher(publisher){}   
 
-        //Getters
+        // Getter functions to retrieve book details
         std::string getTitle(){ return Title; }
         std::string getAuthor(){ return Author; }
         std::string getISBN(){ return ISBN; }
         std::string getPublisher(){ return Publisher; }
 
-        //Setters
+        // Setter functions to modify book details
         void setTitle(std::string bktitle){ Title = bktitle; }
         void setAuthor(std::string bkauthor){ Author = bkauthor; }
         void setISBN(std::string bkisbn){ ISBN = bkisbn; }
--- UML Diagram of Class/testBook.cpp
+++ UML Diagram of Class/testBook.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include "Book.h"
 
-int main(){
+int main(){ // Define the main function
+
+    // Create an instance of the Book class with initial details
     Book book1("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", "978-0-7475-3269-9", "Bloomsbury");
 
+    // Output the initial book details
     std::cout << std::endl;
-    std::cout << book1.getTitle() << std::endl;
-    std::cout << book1.getAuthor() << std::endl;
-    std::cout << book1.getISBN() << std::endl;
-    std::cout << book1.getPublisher() << std::endl;
+    std::cout << "Book #1" << std::endl;
+    std::cout << "Title: " << book1.getTitle() << std::endl;
+    std::cout << "Author: " << book1.getAuthor() << std::endl;
+    std::cout << "ISBN: " << book1.getISBN() << std::endl;
+    std::cout << "Publisher: " << book1.getPublisher() << std::endl;
 
+    // Modify the book details using setter functions
     book1.setTitle("It");
     book1.setAuthor("Stephen King");
     book1.setISBN("0-670-81302-8");
     book1.setPublisher("Viking");
 
+    // Output the modified book details
     std::cout << std::endl;
-    std::cout << book1.getTitle() << std::endl;
-    std::cout << book1.getAuthor() << std::endl;
-    std::cout << book1.getISBN() << std::endl;
-    std::cout << book1.getPublisher() << std::endl;
+    std::cout << "Book #2" << std::endl;
+    std::cout << "Title: " << book1.getTitle() << std::endl;
+    std::cout << "Author: " << book1.getAuthor() << std::endl;
+    std::cout << "ISBN: " << book1.getISBN() << std::endl;
+    std::cout << "Publisher: " << book1.getPublisher() << std::endl;
 
 }
